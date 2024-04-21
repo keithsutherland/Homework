3ed8e9e38b7d0445403068377356f0370a53c15c
keith sutherland <pacneemius7@gmail.com>
2024-04-21 16:39:10 -0500

    Week 2 homework

---
+++ week2/pizzaplacehw.js
@@ -0,0 +1,17 @@
+const pizzaPlace = "PacosPizza";
+
+let numberOfToppings = 7;
+
+console.log(typeof pizzaPlace);
+console.log(typeof numberOfToppings);
+
+console.log(`The name of the pizza resturant is ${pizzaPlace} and we have
+${numberOfToppings} toppings babyyy.`);
+
+if (numberOfToppings <= 10) {
+  console.log("Quality, not quantity");
+}
+
+// for (let numberOfToppings = 7; numberOfToppings <= 10; numberOfToppings++) {
+// {console.log (numberOfToppings);
+// }
