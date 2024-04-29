0c335ba6b95d92587c523128601406fc950f3105
keith sutherland <pacneemius7@gmail.com>
2024-04-28 20:24:15 -0500

    week3 homework to the best of my ability

---
+++ week3.js
@@ -0,0 +1,35 @@
+
+
+const toppings = ["hamburger", "pepperoni", "sausage", "green pepper", "jalapeno", "olive..."];
+
+function greetCustomer() {
+  for (let name of toppings)
+  console.log(name);
+}
+  greetCustomer();
+
+
+function getPizzaOrder (size, crust, lotsToppings) {
+console.log("One " + size + crust + lotsToppings);
+
+}
+console.log(getPizzaOrder("large, " ,"thick, crust pizza with " , toppings+ " coming up!"));
+
+
+  // const pizzaOrder = {
+  //   size: "large",
+  //   crust: "thick",
+  //   wantedToppings: toppings,
+  //   cookingOrNot: "pizza is now cooking",
+  // };
+
+  function preparePizza ([size, crust, toppingsList]) {
+  const order =(preparePizza("large", "thick", "pepperoni")); {
+    console.log(preparePizza);
+    }
+  }
+
+
+
+
+
