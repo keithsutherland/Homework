70504bd17094cce4f39ad2a033dd36c4e11f853c
keith sutherland <pacneemius7@gmail.com>
2024-04-28 20:27:34 -0500

    committing the rest of the files for consistency

--- week2/pizzaplacehw.js
+++ week2/pizzaplacehw.js
@@ -1,19 +1,46 @@
-const pizzaPlace = "PacosPizza";
+// const pizzaPlace = "PacosPizza";
 
-let numberOfToppings = 7;
+// let numberOfToppings = 7;
 
-console.log(typeof pizzaPlace);
-console.log(pizzaPlace);
-console.log(typeof numberOfToppings);
-console.log(numberOfToppings);
+// console.log(typeof pizzaPlace);
+// console.log(pizzaPlace);
+// console.log(typeof numberOfToppings);
+// console.log(numberOfToppings);
 
-console.log(`The name of the pizza resturant is ${pizzaPlace} and we have
-${numberOfToppings} toppings babyyy.`);
+// console.log(`The name of the pizza resturant is ${pizzaPlace} and we have
+// ${numberOfToppings} toppings babyyy.`);
 
-if (numberOfToppings <= 10) {
-  console.log("Quality, not quantity");
-}
+// if (numberOfToppings < 10) {
+//   console.log("Quality, not quantity");
+// }
 
 // for (let numberOfToppings = 7; numberOfToppings <= 10; numberOfToppings++) {
-// {console.log (numberOfToppings);
-// }
+//  total = numberOfToppings % 2;
+// console.log (numberOfToppings);
+//  }
+const konamiCode = "up up down down left right left right a b";
+const sonic3Code = "up down left right start";
+const castlevaniaCOTMCode = "FIREBALL";
+const GTASA = "literally don't know the code";
+
+let cheatCode = GTASA;
+
+// switch (cheatCode) {
+//   case "up up down down left right left right a b":
+//     console.log("Enjoy your 99 lives!");
+//     break;
+//   case "up down left right start":
+//     console.log("welcome to the DEBUG MENU BABYY");
+//     break;
+//   case "FIREBALL":
+//     console.log("You unlocked magician mode!");
+//     break;
+//   default:
+//     console.log("wrong code stoopid!");
+}
+
+const names = ["Tracy", "Jenna", "Kenneth", "Liz", "Jack"];
+
+for (let name of names) {
+  console.log(name);
+}
