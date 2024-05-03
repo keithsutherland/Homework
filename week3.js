10be46ea742e20f137a5c6ac232f73a0ce2236bc
keith sutherland <pacneemius7@gmail.com>
2024-05-02 21:29:48 -0500

    Committing the REAL pizza homework

--- week3.js
+++ week3.js
@@ -1,35 +1,54 @@
 
 
-const toppings = ["hamburger", "pepperoni", "sausage", "green pepper", "jalapeno", "olive..."];
+const pizzaToppings = ["hamburger", "pepperoni", "sausage", "green pepper", "jalapeno", "olive..."];
 
 function greetCustomer() {
-  for (let name of toppings)
-  console.log(name);
-}
-  greetCustomer();
+  let message = 'Welcome to Pacos Pizza, our toppings are: ';
+  console.log(message);
 
+  for (let topping of pizzaToppings) {
+    message += `${topping},`;
 
-function getPizzaOrder (size, crust, lotsToppings) {
-console.log("One " + size + crust + lotsToppings);
-
-}
-console.log(getPizzaOrder("large, " ,"thick, crust pizza with " , toppings+ " coming up!"));
-
-
-  // const pizzaOrder = {
-  //   size: "large",
-  //   crust: "thick",
-  //   wantedToppings: toppings,
-  //   cookingOrNot: "pizza is now cooking",
-  // };
-
-  function preparePizza ([size, crust, toppingsList]) {
-  const order =(preparePizza("large", "thick", "pepperoni")); {
-    console.log(preparePizza);
-    }
   }
+greetCustomer();
 
+function getPizzaOrder(size, crust, ...toppings) {
+let message = `One ${size} ${crust} crust pizza with ${toppings} ... coming up!`
 
+for (let topping of toppings) {
+  message += `${topping},`;
 
+message += ", ... coming up";
+console.log(message);
 
+return [size, crust, toppings];
+}
+getPizzaOrder('medium','thin','pepperoni', 'ham');
 
+function preparePizza([size, crust, toppings]) {
+console.log('..Cooking pizza...');
+
+return {
+  size:size,
+  crust:crust,
+  toppings:toppings
+  }
+}
+
+function servePizza(pizza) {
+let message = `Order up! Here's your ${pizza.size} ${pizza.crust} crust pizza with`;
+for (let topping of pizza.toppings) {
+  message += `${topping},`;
+}
+
+message += "...Enjoy!";
+console.log(message);
+
+return pizza;
+}
+
+const orderArray = getPizzaOrder('hamburger', 'pepperoni', 'sausage', 'green pepper', 'jalapeno', 'olive');
+const orderObject = preparePizza(orderArray);
+const orderSameObject = servePizza(orderObject);
+//2 different ways here to wrap the functions in themselves
+servePizza(preparePizza(getPizzaOrder('hamburger', 'pepperoni', 'sausage', 'green pepper', 'jalapeno', 'olive')))
